de69ad84eca187e246bbb6193e6e43753362ca9a
michbur <michalburdukiewicz@gmail.com>
2018-10-03 13:22:11 +0200

    added new examples

--- Materialy/ggplot2-intro.R
+++ Materialy/ggplot2-intro.R
@@ -61,19 +61,24 @@ library(gridExtra)
 grid.arrange(p1, p2, nrow = 1)
 
 
-# 2. Wielowarstwowe wykresy
+# 2. Wielowarstwowe wykresy ------------------------
 # geometrie mozna laczyc
 ggplot(data = countries, aes(x = continent, y = death.rate)) +
   geom_boxplot() +
   geom_point()
 
+set.seed(15390)
+ggplot(data = countries, aes(x = continent, y = death.rate)) +
+  geom_point(position = "jitter") +
+  geom_boxplot(outlier.color = NA) # w celu unikniecia dublowania outlierow z geom_point
+
 # wielowymiarowe i wielowarstwowe wykresy
 
 continents <- group_by(countries, continent) %>% 
   na.omit %>% 
-  summarise(death.rate = sum(death.rate),
-            birth.rate = sum(birth.rate, na.rm = TRUE),
-            population = sum(population),
+  summarise(death.rate = mean(death.rate),
+            birth.rate = mean(birth.rate, na.rm = TRUE),
+            population = mean(population),
             n.countries = length(country))
 
 ggplot(continents, aes(x = birth.rate, y = death.rate, size = population)) +
@@ -84,6 +89,10 @@ ggplot(continents, aes(x = birth.rate, y = death.rate, size = population, color 
 
 ggplot(continents, aes(x = birth.rate, y = death.rate, size = population, color = n.countries)) +
   geom_point()
+
+ggplot(continents, aes(x = birth.rate, y = death.rate, size = population, color = n.countries)) +
+  geom_point() +
+  scale_size_continuous(range = c(4, 12))
 
 # geometrie automatycznie wykorzystują okreslone atrybuty graficzne.
 # w tym przypadku atrybut size automatycznie jest przypisany zarowno do geom_point i geom_text
@@ -101,7 +110,12 @@ ggplot(continents, aes(x = birth.rate, y = death.rate, size = population, label 
   geom_point() +
   geom_text_repel(size = 5, force = 1)
 
-ggplot(continents, aes(x = birth.rate, y = death.rate, size = population, label = continent, color = n.countries)) +
+ggplot(continents, aes(x = birth.rate, y = death.rate, size = population, label = continent)) +
+  geom_point() +
+  geom_label_repel(size = 5, force = 1)
+
+ggplot(continents, aes(x = birth.rate, y = death.rate, size = population, label = continent, 
+                       color = n.countries)) +
   geom_point() +
   geom_text_repel(size = 5, force = 1, color = "black") 
 
@@ -114,12 +128,20 @@ ggplot(continents, aes(x = birth.rate, y = death.rate, size = population,
 
 library(ggbeeswarm)
 ggplot(data = countries, aes(x = continent, y = death.rate)) +
+  geom_quasirandom(method = "smiley")
+
+ggplot(data = countries, aes(x = 1, y = death.rate, color = continent)) +
   geom_quasirandom()
 
 ggplot(data = countries, aes(x = continent, y = death.rate)) +
   geom_quasirandom() +
   geom_point(data = group_by(countries, continent) %>% summarise(mean.death.rate = mean(death.rate)),
-             aes(x = continent, y = mean.death.rate), color = "red", size = 4)
+             aes(y = mean.death.rate), color = "red", size = 4)
+
+ggplot(data = countries, aes(x = continent, y = death.rate)) +
+  geom_quasirandom() +
+  geom_point(data = group_by(countries, continent) %>% summarise(mean.death.rate = mean(death.rate)),
+             aes(x = continent, y = mean.death.rate), color = "red", size = 4, inherit.aes = FALSE)
 
 # 3. Czytelnosc wykresow ------------------------------ 
 # wykresy gestosci bywaja czytelniejsze niz punktowe w przypadku duzej liczby punktow
@@ -127,18 +149,22 @@ ggplot(countries, aes(x = birth.rate, y = death.rate, color = continent)) +
   geom_point()
 
 ggplot(countries, aes(x = birth.rate, y = death.rate)) +
-  geom_density_2d(color = "black", contour = TRUE)
+  geom_density_2d(color = "black", contour = TRUE) +
+  geom_point()
 
 ggplot(countries, aes(x = birth.rate, y = death.rate, fill = continent)) +
   geom_density_2d(color = "black", contour = TRUE)
 
 # wykorzystujemy stat, a nie geom, poniewaz interesuje nas inna geometria (polygon)
 ggplot(countries, aes(x = birth.rate, y = death.rate, fill = continent)) +
-  stat_density2d(aes(alpha = ..level..), color = "black", contour = TRUE, geom = "polygon")
-
+  stat_density_2d(aes(alpha = ..level..), color = "black", contour = TRUE, geom = "polygon")
 
 # czasami to nie wystarcza i trzeba uciec się do paneli
 ggplot(countries, aes(x = birth.rate, y = death.rate, fill = continent)) +
+  stat_density2d(aes(alpha = ..level..), color = "black", contour = TRUE, geom = "polygon") +
+  facet_wrap(~ continent)
+
+ggplot(countries, aes(x = birth.rate, y = death.rate)) +
   stat_density2d(aes(alpha = ..level..), color = "black", contour = TRUE, geom = "polygon") +
   facet_wrap(~ continent)
 
@@ -151,19 +177,27 @@ ggplot(countries, aes(x = birth.rate, y = death.rate)) +
   geom_point() + 
   geom_smooth() 
 
+ggplot(countries, aes(x = birth.rate, y = death.rate, color = continent)) +
+  geom_point() + 
+  geom_smooth() 
+
+ggplot(countries, aes(x = birth.rate, y = death.rate)) +
+  geom_point() + 
+  geom_smooth(se = FALSE) 
+
 # skrajne przypadki mozna podpisac
 na.omit(countries) %>% 
-  mutate(label = ifelse(birth.rate %in% range(birth.rate) | death.rate %in% range(death.rate),
+  mutate(label.for.plot = ifelse(birth.rate %in% range(birth.rate) | death.rate %in% range(death.rate),
                         country, "")) %>% 
-  ggplot(aes(x = birth.rate, y = death.rate, label = label)) +
+  ggplot(aes(x = birth.rate, y = death.rate, label = label.for.plot)) +
   geom_point() +
   geom_text_repel()
-
 
 # 4. Zadania ---------------------------------------------------
 mieszkania <- read.csv(file = "https://raw.githubusercontent.com/STWUR/STWUR-2017-06-07/master/data/mieszkania_dane.csv", 
                        encoding = "UTF-8") %>% 
   na.omit
 
-# I. Na jednym wykresie przedstaw rozklad cen i wieku mieszkan dla a) calego Wroclawia b) poszczegolnych dzielnic.
+# I. Na jednym wykresie przedstaw rozklad cen i wieku mieszkan dla a) calego Wroclawia b) 
+# poszczegolnych dzielnic.
 # II. Przedstaw zaleznosc ceny za metr^2 od dzielnicy, pietra i roku zbudowania mieszkania.
