a5b73ba7e1a886c8aca9f853cde2c4584e5d8aca
michbur <michalburdukiewicz@gmail.com>
2018-10-03 11:25:35 +0200

    added intro to ggplot2

---
+++ Materialy/ggplot2-intro.R
@@ -0,0 +1,149 @@
+library(ggplot2)
+library(SmarterPoland)
+library(dplyr)
+
+# 1. Mapowanie --------------------------------------------
+# przypisanie zrodla danych
+ggplot(data = countries)
+
+# mapowanie 
+# tu przypisujemy dane do osi, zwroc uwage na zmiane wartosci na osi x
+ggplot(data = countries, aes(x = birth.rate, y = death.rate))
+ggplot(data = countries, aes(x = continent, y = death.rate))
+
+# dodawanie geometrii
+ggplot(data = countries, aes(x = birth.rate, y = death.rate)) +
+  geom_point()
+
+ggplot(data = countries, aes(x = continent, y = death.rate)) +
+  geom_point()
+
+ggplot(data = countries, aes(x = continent, y = death.rate)) +
+  geom_boxplot()
+
+ggplot(data = countries, aes(x = continent, y = death.rate)) +
+  geom_dotplot(binaxis = "y", stackdir = "center")
+
+ggplot(data = countries, aes(x = continent, y = death.rate)) +
+  geom_violin()
+
+# dane mozna przypisywac nie tylko do osi, ale rowniez do innych atrybutow graficznych 
+ggplot(data = countries, aes(x = birth.rate, y = death.rate, color = continent)) +
+  geom_point()
+
+# niektore geometrie wymagaja konkretnych atrybutow graficznych
+ggplot(data = countries, aes(x = continent)) +
+  geom_bar()
+
+ggplot(data = countries, aes(x = death.rate)) +
+  geom_density()
+
+ggplot(data = countries, aes(x = death.rate, fill = continent)) +
+  geom_density(alpha = 0.2)
+
+# 2. Wielowarstwowe wykresy
+# geometrie mozna laczyc
+ggplot(data = countries, aes(x = continent, y = death.rate)) +
+  geom_boxplot() +
+  geom_point()
+
+# wielowymiarowe i wielowarstwowe wykresy
+
+continents <- group_by(countries, continent) %>% 
+  na.omit %>% 
+  summarise(death.rate = sum(death.rate),
+            birth.rate = sum(birth.rate, na.rm = TRUE),
+            population = sum(population),
+            n.countries = length(country))
+
+ggplot(continents, aes(x = birth.rate, y = death.rate, size = population)) +
+  geom_point()
+
+ggplot(continents, aes(x = birth.rate, y = death.rate, size = population, color = continent)) +
+  geom_point()
+
+ggplot(continents, aes(x = birth.rate, y = death.rate, size = population, color = n.countries)) +
+  geom_point()
+
+# geometrie automatycznie wykorzystują okreslone atrybuty graficzne.
+# w tym przypadku atrybut size automatycznie jest przypisany zarowno do geom_point i geom_text
+ggplot(continents, aes(x = birth.rate, y = death.rate, size = population, label = continent)) +
+  geom_point() +
+  geom_text(vjust = -1)
+
+ggplot(continents, aes(x = birth.rate, y = death.rate, size = population, label = continent)) +
+  geom_point() +
+  geom_text(vjust = -1, size = 5)
+
+# wiele uzytecznych geometrii mozna znalezc w pakietach rzoszerzajacych ggplot2
+library(ggrepel)
+ggplot(continents, aes(x = birth.rate, y = death.rate, size = population, label = continent)) +
+  geom_point() +
+  geom_text_repel(size = 5, force = 1)
+
+ggplot(continents, aes(x = birth.rate, y = death.rate, size = population, label = continent, color = n.countries)) +
+  geom_point() +
+  geom_text_repel(size = 5, force = 1, color = "black") 
+
+ggplot(continents, aes(x = birth.rate, y = death.rate, size = population, 
+                       label = paste0(continent, "; ", n.countries, " countries"))) +
+  geom_point() +
+  geom_text_repel(size = 4, force = 1, color = "black") 
+
+# na jednym rysunku mozna przedstawic dane z wiecej niz jednego zrodla
+
+library(ggbeeswarm)
+ggplot(data = countries, aes(x = continent, y = death.rate)) +
+  geom_quasirandom()
+
+ggplot(data = countries, aes(x = continent, y = death.rate)) +
+  geom_quasirandom() +
+  geom_point(data = group_by(countries, continent) %>% summarise(mean.death.rate = mean(death.rate)),
+             aes(x = continent, y = mean.death.rate), color = "red", size = 4)
+
+# 3. Czytelnosc wykresow ------------------------------ 
+# wykresy gestosci bywaja czytelniejsze niz punktowe w przypadku duzej liczby punktow
+ggplot(countries, aes(x = birth.rate, y = death.rate, color = continent)) +
+  geom_point()
+
+ggplot(countries, aes(x = birth.rate, y = death.rate)) +
+  geom_density_2d(color = "black", contour = TRUE)
+
+ggplot(countries, aes(x = birth.rate, y = death.rate, fill = continent)) +
+  geom_density_2d(color = "black", contour = TRUE)
+
+# wykorzystujemy stat, a nie geom, poniewaz interesuje nas inna geometria (polygon)
+ggplot(countries, aes(x = birth.rate, y = death.rate, fill = continent)) +
+  stat_density2d(aes(alpha = ..level..), color = "black", contour = TRUE, geom = "polygon")
+
+
+# czasami to nie wystarcza i trzeba uciec się do paneli
+ggplot(countries, aes(x = birth.rate, y = death.rate, fill = continent)) +
+  stat_density2d(aes(alpha = ..level..), color = "black", contour = TRUE, geom = "polygon") +
+  facet_wrap(~ continent)
+
+# wykresy zyskuja poprzez pokazanie linii trendu
+
+ggplot(countries, aes(x = birth.rate, y = death.rate)) +
+  geom_point() 
+
+ggplot(countries, aes(x = birth.rate, y = death.rate)) +
+  geom_point() + 
+  geom_smooth() 
+
+# skrajne przypadki mozna podpisac
+na.omit(countries) %>% 
+  mutate(label = ifelse(birth.rate %in% range(birth.rate) | death.rate %in% range(death.rate),
+                        country, "")) %>% 
+  ggplot(aes(x = birth.rate, y = death.rate, label = label)) +
+  geom_point() +
+  geom_text_repel()
+
+
+# 4. Zadania ---------------------------------------------------
+mieszkania <- read.csv(file = "https://raw.githubusercontent.com/STWUR/STWUR-2017-06-07/master/data/mieszkania_dane.csv", 
+                       encoding = "UTF-8") %>% 
+  na.omit
+
+# I. Na jednym wykresie przedstaw rozklad cen i wieku mieszkan dla a) calego Wroclawia b) poszczegolnych dzielnic.
+# II. Przedstaw zaleznosc ceny za metr^2 od dzielnicy, pietra i roku zbudowania mieszkania.
