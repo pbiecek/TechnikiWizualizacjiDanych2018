199d01b2613a16fcf10d4e8552b908e05bf70270
GoniaW <32526340+GoniaW@users.noreply.github.com>
2018-10-10 13:14:58 +0200

    Delete GosiaWachulec.R

--- proton/gr2/GosiaWachulec.R
+++
@@ -1,9 +0,0 @@
-install.packages("BetaBit")
-install.packages("data.table")
-library(data.table)
-library(BetaBit)
-proton()
-
-df <- data.table(employees)
-
-df1 <- df[name == "John" & surname == "Insecure"]
