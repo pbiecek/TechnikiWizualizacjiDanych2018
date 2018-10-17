cf27b56e44a00b7ea8576c5a8b8d693a4e2e8ecc
291139 werner olaf <wernero@p20104.mini.pw.edu.pl>
2018-10-17 13:15:25 +0200

    inny

--- innywykres.R
+++ innywykres.R
@@ -1,18 +1,10 @@
 library(ggplot2)
-library(ggplot2)
-dane<-data.frame(nazwa=factor(c("Podstawowe nieukończone","Podstawowe ukończone","Zasadnicze zawodowe","Średnie ogólno-kształcące",'Średnie zawodowe','Policealne','Wyższe')),
+dane<-data.frame(nazwa=factor(c("Podstawowe nieukończone","Podstawowe ukończone","Zasadnicze zawodowe","Średnie ogólno-kształcące",'Średnie zawodowe','Policealne','Wyższe'),levels = c("Podstawowe nieukończone","Podstawowe ukończone","Zasadnicze zawodowe","Średnie ogólno-kształcące",'Średnie zawodowe','Policealne','Wyższe')),
                  procenty=c(2.4,37.9,38.8,2.2,15.9,1.0,1.6))
-ggplot(dane,aes(x=nazwa,y=procenty,fill=nazwa)) + geom_col(width=0.7) + 
-  geom_text(aes(label=as.character(procenty),y=procenty+1),size=5,fontface="bold") + guides(fill=FALSE) + 
-  coord_fixed(ratio = 0.25) +
-  labs(title="Użytkownicy pracujący wyłącznie lub głównie w swoim gospodarstwie rolnym (na działce rolnej) według poziomu wykształcenia w 2002 r.",y="",x="",subtitle="(W PROC.)") +
-  theme_minimal()+theme(panel.border = element_blank(),
-                        panel.grid.major = element_blank(),
-                        panel.grid.minor = element_blank(),
-                        axis.text.y = element_blank(),
-                        axis.text = element_text(face = "bold",size = rel(1.5)),
-                        plot.title = element_text(size = rel(1.75),hjust=-.1,face="bold")
-                        
-  )
-                        
-  )
+
+
+ggplot(data=dane, aes(x=nazwa, y=procenty)) +
+  geom_bar(stat="identity") + labs(title="Użytkownicy pracujący wyłącznie lub głównie w swoim\n gospodarstwie rolnym (na działce rolnej)\n według poziomu wykształcenia w 2002 r.",x=" ") + coord_flip() +theme(title = element_text(vjust = 0.5))
+  
+  
+
